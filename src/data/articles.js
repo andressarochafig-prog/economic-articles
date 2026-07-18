7986c51959cb0ba85906eb15dd23f2c7a8e9776d
andressarochafig-prog <andressarocha.fig@gmail.com>
2026-07-18 03:43:12 -0300

    corrige texto do artigo

--- src/data/articles.js
+++ src/data/articles.js
@@ -16,7 +16,7 @@ export const articles = [
       },
       {
         type: "paragraph",
-        text: "A frase dita pela célebre personagem nordestina de Clarice Lispector resume uma realidade que vem crescendo exponencialmente. Em um mundo marcado por uma das maiores contradições do desenvolvimento econômico moderno – no qual a produtividade agrícola e os avanços biotecnológicos atingem recordes históricos –, a escolha alimentar de um indivíduo vem deixando ser direcionada por seus desejos (seja por ter uma vida mais saudável ou porque sente vontade de consumir tal alimento) e passa a ser ditada por seu poder aquisitivo. Essa escolha, é, portanto, imposta pelo custo marginal de sobrevivência. Comer, no Brasil contemporâneo, tornou-se um ato de restrição orçamentária.",
+        text: "A frase dita pela célebre personagem nordestina de Clarice Lispector resume uma realidade que vem crescendo exponencialmente. Em um mundo marcado por uma das maiores contradições do desenvolvimento econômico moderno – no qual a produtividade agrícola e os avanços biotecnológicos atingem recordes históricos –, a escolha alimentar de um indivíduo vem deixando ser direcionada por seus desejos (seja por ter uma vida mais saudável ou porque sente vontade de consumir tal alimento) e passa a ser ditada por seu poder aquisitivo. Essa escolha, é, portanto, limitada pela renda disponível. Comer, no Brasil contemporâneo, tornou-se um ato de restrição orçamentária.",
       },
       {
         type: "heading",
@@ -28,7 +28,7 @@ export const articles = [
       },
       {
         type: "paragraph",
-        text: "Diferente de indústrias que podem acelerar ou desacelerar suas máquinas conforme exige a demanda no mercado, a agricultura depende do tempo biológico de cultivo e das condições climáticas. Esses eventos meteorológicos e geopolíticos afetam diretamente a produção de alimentos agrícolas, cuja elasticidade-preço da oferta é inelástica no curto prazo. Dessa forma, diante de uma quebra de safra, a curva de oferta se desloca abruptamente para a esquerda, forçando uma rápida escalada de preços que é repassada integralmente ao consumidor final.",
+        text: "Diferente de indústrias que podem acelerar ou desacelerar suas máquinas conforme exige a demanda no mercado, a agricultura depende do tempo biológico de cultivo e das condições climáticas. Esses eventos meteorológicos e geopolíticos afetam diretamente a produção de alimentos agrícolas, cuja oferta apresenta baixa elasticidade-preço no curto prazo. Dessa forma, diante de uma quebra de safra, a curva de oferta se desloca abruptamente para a esquerda, forçando uma rápida escalada de preços que é repassada parcialmente ao consumidor final.",
       },
       {
         type: "figure",
@@ -47,7 +47,7 @@ export const articles = [
       },
       {
         type: "paragraph",
-        text: "Primeiramente, o custo logístico atua como um imposto invisível sobre os produtos frescos. A disparada dos preços dos combustíveis e do frete – exemplificada pelo aumento de até 62% no frete agrícola registrado no Mato Grosso no início de 2025 – eleva drasticamente o custo de transporte de produtos perecíveis (hortaliças, legumes e frutas são os principais alimentos que sofrem alto índice de perda pós-colheita). Além disso, a política de desativação histórica dos estoques reguladores de alimentos (geridos pela Companhia Nacional de Abastecimento – Conab) eliminou o mecanismo estatal de amortecimento de preços, então, uma vez que o governo não dispõe de estoque para intervir nos momentos de escassez, o mercado fica totalmente exposto à volatilidade climática e cambial.",
+        text: "Primeiramente, o custo logístico atua como um imposto invisível sobre os produtos frescos. A disparada dos preços dos combustíveis e do frete – exemplificada pelo aumento de até 62% no frete agrícola registrado no Mato Grosso no início de 2025, de acordo com o Abralog – eleva drasticamente o custo de transporte de produtos perecíveis (hortaliças, legumes e frutas são os principais alimentos que sofrem alto índice de perda pós-colheita). Além disso, a política de desativação histórica dos estoques reguladores de alimentos (geridos pela Companhia Nacional de Abastecimento – Conab) reduziu de forma significativa o mecanismo estatal de amortecimento de preços, então, uma vez que o governo dispõe de capacidade limitada de intervenção por meio dos estoques públicos nos momentos de escassez, o mercado fica exposto à volatilidade climática e cambial.",
       },
       {
         type: "figure",
@@ -58,7 +58,7 @@ export const articles = [
       },
       {
         type: "paragraph",
-        text: "Outro ponto crucial de se explicitar é o impacto da política monetária. Sob uma Selic elevada, o crédito permanece caro, ocasionando em um adiamento de investimentos produtivos, como a compra de maquinários e a produção em larga escala. Adicionalmente, a valorização do dólar eleva os custos de produção interno ao encarecer os insumos dolarizados amplamente utilizados na agricultura (como fertilizantes químicos, pesticidas e maquinário). Esse processo faz com que o custo marginal seja pressionado, configurando o fenômeno conhecido como Transmissão Cambial.",
+        text: "Outro ponto crucial de se explicitar é o impacto da política monetária. Sob uma Selic elevada, o crédito permanece caro, ocasionando em um adiamento de investimentos produtivos, como a compra de maquinários e a ampliação da produção. Adicionalmente, a valorização do dólar eleva os custos de produção interno ao encarecer os insumos dolarizados amplamente utilizados na agricultura (como fertilizantes químicos, pesticidas e maquinário). Esse processo faz com que o custo marginal seja pressionado, configurando o fenômeno conhecido como Transmissão Cambial.",
       },
       {
         type: "figure",
@@ -73,11 +73,11 @@ export const articles = [
       },
       {
         type: "paragraph",
-        text: "Ademais, enquanto questões climáticas e de logística já são tradicionalmente mapeadas pelos produtores, nos últimos anos evidenciou-se um componente subjetivo de peso: a ascensão da cultura ‘Wellness’ e a busca incessante por uma vida saudável. Impulsionado pelo forte apelo midiático e digital, as grandes corporações alimentícias rapidamente identificaram a emergência de um público-alvo disposto a pagar um prêmio de preço por produtos rotulados como saudável ou orgânicos. Esse comportamento desloca a curva de demanda para a direita, evidenciando que, no que tange à saúde e ao bem-estar, a elasticidade-preço da demanda nesse nicho de maior renda tornou-se altamente inelástica, ou seja, para esse público a questão financeira deixa de ser o fator decisivo, uma vez que ela comprará o produto quase independente de quanto ele esteja custando, porque, agora, deixa de ser uma questão financeira ou apenas de saúde, mas também de pertencimento a esse grupo que vem crescendo.",
+        text: "Ademais, enquanto questões climáticas e de logística já são tradicionalmente mapeadas pelos produtores, nos últimos anos evidenciou-se um componente comportamental relevante: a ascensão da cultura ‘Wellness’ e a busca incessante por uma vida saudável. Impulsionado pelo forte apelo midiático e digital, as grandes corporações alimentícias rapidamente identificaram a emergência de um público-alvo disposto a pagar um prêmio de preço por produtos rotulados como saudável ou orgânicos. Esse comportamento desloca a curva de demanda para a direita, evidenciando que, no que tange à saúde e ao bem-estar, para esse público a questão financeira deixa de ser o fator decisivo, uma vez que, agora, deixa de ser uma questão financeira ou apenas de saúde, mas também de pertencimento a esse grupo que vem crescendo.",
       },
       {
         type: "paragraph",
-        text: "Esse movimento, contudo, gera uma forte barreira econômica de entrada para as classes mais desfavorecidas economicamente e pressiona toda a cadeia de precificação nacional. Em termos mais macroeconômicos, o rápido avanço dessa onda de demanda confronta-se com a rigidez de curto prazo da oferta de alimentos naturais. Essa situação gera um excesso de demanda que, diante da escassez imediata de estoques, força a elevação dos preços de mercado para um novo patamar de equilíbrio, atingindo o bolso dos consumidores brasileiros. Esse aumento de preços atua tanto como um mecanismo de racionamento de mercado, quanto como garantia de cobertura dos custos marginais dos produtores, impedindo o colapso da oferta, mas inviabilizando o acesso democrático ao alimento saudável.",
+        text: "Esse movimento, contudo, gera uma forte barreira econômica de entrada para as classes mais desfavorecidas economicamente. Em termos mais macroeconômicos, o rápido avanço dessa onda de demanda confronta-se com a rigidez de curto prazo da oferta de alimentos naturais. Essa situação gera um excesso de demanda que, diante da escassez imediata de estoques, força a elevação dos preços de mercado para um novo patamar de equilíbrio, atingindo o bolso dos consumidores brasileiros. Esse aumento de preços atua criando incentivos para uma expansão futura de oferta, mas inviabilizando o acesso democrático ao alimento saudável.",
       },
       {
         type: "heading",
@@ -89,11 +89,11 @@ export const articles = [
       },
       {
         type: "paragraph",
-        text: "Diante de uma renda que sobra cada vez mais curta no fim do mês e de preços de produtos in natura cada vez mais salgados, ocorre o chamado efeito substituição por qualidade inferior. As famílias economicamente vulneráveis são forçadas a substituir frutas, vegetais e carnes magras por alimentos ultraprocessados (ricos em açúcares, sódio e gorduras hidrogenadas), que são altamente calóricos, fáceis de armazenar e de custo significativamente menor graças à escala industrial de produção. A consequência direta é a consolidação de uma barreira social silenciosa: as classes mais ricas compram saúde através da nutrição, enquanto as classes mais pobres compram apenas calorias baratas para saciar a fome imediata, perpetuando o ciclo de insegurança alimentar, obesidade e doenças crônicas.",
+        text: "Diante de uma renda disponível cada vez mais curta no fim do mês e de preços de produtos in natura cada vez mais salgados, ocorre uma substituição para alimentos de menor qualidade. As famílias economicamente vulneráveis são forçadas a substituir frutas, vegetais e carnes magras por alimentos ultraprocessados (ricos em açúcares, sódio e gorduras hidrogenadas), que são altamente calóricos, fáceis de armazenar e de custo significativamente menor graças à escala industrial de produção. A consequência direta é a consolidação de uma barreira social silenciosa: as classes mais ricas compram saúde através da nutrição, enquanto as classes mais pobres compram apenas calorias baratas para saciar a fome imediata, perpetuando o ciclo de insegurança alimentar, obesidade e doenças crônicas.",
       },
       {
         type: "paragraph",
-        text: "Em suma, a inflação dos alimentos saudáveis no Brasil contemporâneo não é um fenômeno puramente mercadológico, mas o reflexo de gargalos logísticos estruturais, escolhas de política monetária e da apropriação do bem-estar como nicho de luxo. Enquanto a alimentação de qualidade for tratada como um bem de consumo elástico e exclusivo para as faixas de alta renda, a desigualdade social continuará a ser medida pela balança e pelo prato. Democratizar o acesso ao alimento in natura exige mais do que o livre mercado; demanda o resgate de políticas públicas de abastecimento e a compreensão de que nutrir-se com dignidade é, antes de tudo, um direito fundamental.",
+        text: "Em suma, a inflação dos alimentos saudáveis no Brasil contemporâneo não é um fenômeno puramente mercadológico, mas o reflexo de gargalos logísticos estruturais, escolhas de política monetária e da apropriação do bem-estar como nicho de luxo. Enquanto a alimentação de qualidade for tratada como um bem de luxo, a desigualdade social continuará a ser medida pela balança e pelo prato. Democratizar o acesso ao alimento in natura exige mais do que o livre mercado; demanda o resgate de políticas públicas de abastecimento e a compreensão de que nutrir-se com dignidade é, antes de tudo, um direito fundamental.",
       },
     ],
   },
